0c9927c5d0d2f0b9ab6643de0794ff9099f521cc
nks900 <88540881+nks900@users.noreply.github.com>
2021-11-18 17:22:00 +0530

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -2,11 +2,12 @@
 
 apt install deepin-terminal -y
 apt install qbittorrent -y
+apt install telegram-desktop -y
 apt install mpv -y
 git clone https://github.com/pystardust/ani-cli.git
 cd ani-cli
 sudo make
 cd ..
-apt install telegram-desktop -y
-deepin-terminal
+
+
 
